fc77c4e96970a0d4fb2d818ca61bda7e705a0e30
Alicja Gosiewska <alicjagosiewska@gmail.com>
2017-10-07 20:51:56 +0200

    aktualizuj skrypt podstawa blok5

--- carrots_workshop/podstawa/blok_5/blok_5_czesc_1_wykresy.R
+++ carrots_workshop/podstawa/blok_5/blok_5_czesc_1_wykresy.R
@@ -1,5 +1,10 @@
 # Po co nam wizualizacja?
 
+vec1 <- c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5, 5, 5, 5, 5, 5, 4.5, 4, 3, 2, 3, 4, 5, 3.5, 2.5, 2.5, 3.5, 4.5, 1.5)
+vec2 <- c(0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5.5, 5, 4.5, 4, 3.5, 3, 3, 3, 3, 3, 2, 1, 0, 3, 3, 2.5, 1.5, 0.5, 3)
+
+plot(vec1, vec2, pch = 19)
+
 ###############
 #Podstawowe wykresy
 View(women)
@@ -18,11 +23,15 @@ plot(x=women$weight, y=women$height, type="l", main="Dane dotyczące kobiet w US
 plot(x=women$weight, y=women$height, type="l", main="Dane dotyczące kobiet w USA", xlab = "waga", ylab="wiek")
 ## kolory
 plot(x=women$weight, y=women$height, type="l", main="Dane dotyczące kobiet w USA", xlab = "waga", ylab="wiek", col="red")
+plot(x=women$weight, y=women$height, type="l", main="Dane dotyczące kobiet w USA", xlab = "waga", ylab="wiek", col="#607b8b")
 
 
 ###ZADANIE
-#Wczytaj zbiór.
-#Popróbowac rysowania róznych kolumn, dodanie kolorków i tytułów.
+#Wczytaj zbiór z pliku dane.csv
+#Spróbuj narysować zawarte w nim dane. 
+#Pobaw się typem wykresu, kolorami, nadaj tytuł
+#Pamiętaj o ustawieniu ścieżki - funkcja setwd()
+
 
 dane <- read.csv("dane.csv")
 
@@ -37,11 +46,13 @@ library(PogromcyDanych)
 View(imiona_warszawa)
 imiona <- filter(imiona_warszawa, rok==2012, miesiac==5)
 imiona_mentorki <- filter(imiona, imie %in% c("Aleksandra", "Alicja", "Marta", "Martyna", "Paulina"))
-barplot(imiona_mentorki$liczba, names.arg=imiona_mentorki$imie, main="Imiona mentorek")
+
+barplot(height = imiona_mentorki$liczba, names.arg=imiona_mentorki$imie, main="Imiona mentorek")
+?barplot
 
 ### ZADANIE
   #pobawić się ze zbiorem koty_ptaki,
-  #narysować histogram(gatunek - waga, żwotność, długość)
+  #narysować wyskres słupkowy (gatunek - waga, żwotność, długość)
   #narysować wykres waga vs długość
   #Pamietajcie o tytułach i podpisach osi, kolory mile widziane ;)
 
@@ -57,12 +68,13 @@ View(koty_ptaki)
 View(maturaExam)
 
 matura2013 <- filter(maturaExam, rok==2013, przedmiot=="j. polski")
+
 hist(matura2013$punkty)
 hist(matura2013$punkty, breaks = 100)
 
 ### ZADANIE
   #Na podstawie zbioru maturaExam sprawdź, czy również w przypadku matury z matematyki
-  #występuje pik w okolicach 15 punktów.
+  #występuje pik w okolicach 30 procent.
   #Pamiętaj o tytule i podpisaniu osi wykresu.
 
 
